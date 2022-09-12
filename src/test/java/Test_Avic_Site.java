eb42f9eeb8889fda28ed104582aa37200c0c94cd
Irina-Berizinska <80217790+Irina-Berizinska@users.noreply.github.com>
2022-09-12 16:33:07 +0300

    fix6

--- src/test/java/Test_Avic_Site.java
+++ src/test/java/Test_Avic_Site.java
@@ -11,7 +11,17 @@ import java.time.Duration;
 public class Test_Avic_Site {
     private static ChromeDriver chromeDriver;
     WebDriverWait wait;
-    WebElement clickOnElement(String query, String text, long millis) throws InterruptedException {
+    WebElement clickOnElement(String query, long millis) throws InterruptedException {
+        WebElement element = chromeDriver.findElement(By.xpath(query));
+        element.click();
+
+        if (millis > 0) {
+            Duration.ofMillis(millis);
+        }
+
+        return element;
+    }
+    WebElement inputData(String query, String text, long millis) throws InterruptedException {
         WebElement element = chromeDriver.findElement(By.xpath(query));
         if (StringUtils.isNotBlank(text)) {
             element.sendKeys(text);
@@ -19,11 +29,13 @@ public class Test_Avic_Site {
         element.click();
 
         if (millis > 0) {
-            Thread.sleep(millis);
+            Duration.ofMillis(millis);
         }
 
         return element;
     }
+
+
     @BeforeAll
     public static void setup() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
@@ -39,7 +51,7 @@ public class Test_Avic_Site {
     @Test
     @DisplayName("Check Display Banner")
     public void checkBanner() {
-        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(35));
+        wait = new WebDriverWait(chromeDriver, Duration.ofMillis(35000));
                 wait.until(driver -> driver.findElement(By.xpath("(//div[@id='js_popUp'])")).isDisplayed());
 
     }
@@ -47,59 +59,37 @@ public class Test_Avic_Site {
     @Test
     @DisplayName("Wait Element")
     public void waitElement() {
-    wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
+    wait = new WebDriverWait(chromeDriver, Duration.ofMillis(3500));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/ua/iphone/seriya--iphone-13-promax']")));
     }
 
     @Test
     @DisplayName("Search Button")
     public void searchButton() throws InterruptedException {
-//        element = chromeDriver.findElement(By.id("input_search"));
-//        element.sendKeys("Apple MacBook Air");
-        clickOnElement("//*[@id='input_search']", "Apple MacBook Air", 5000);
-//        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
-        clickOnElement("//button[@class='button-reset search-btn']", null, 5000);
-//        element = chromeDriver.findElement(By.xpath("//button[@class='button-reset search-btn']"));
-//        element.click();
-//        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
+        inputData("//*[@id='input_search']", "Apple MacBook Air", 5000);
+        clickOnElement("//button[@class='button-reset search-btn']",  5000);
     }
 
     @Test
     @DisplayName("Verify Test")
     public void verifyButtonName() throws InterruptedException {
-//        element = chromeDriver.findElement(By.xpath("//*[@id='mm-0']/header/div[2]/div/div[4]/a[1]"));
-//        element.click();
-        clickOnElement("//*[@id='mm-0']/header/div[2]/div/div[4]/a[1]", null, 3000);
-//        element = chromeDriver.findElement(By.xpath("//input[@class='validate']"));
-//        element.sendKeys("0636686093");
-//        element.click();
-//        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(3));
-        clickOnElement("//input[@class='validate']", "0636686093", 3000);
-//
-//        element = chromeDriver.findElement(By.xpath("//input[@class='validate show-password']"));
-//        element.sendKeys("1234@1234");
-//        element.click();
-        clickOnElement("//input[@class='validate show-password']", "1234@1234", 3000);
+        clickOnElement("//*[@id='mm-0']/header/div[2]/div/div[4]/a[1]", 5000);
 
-//        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(3));
-//        element = chromeDriver.findElement(By.xpath("//div[@class='checkbox']"));
-//        element.click();
+        inputData("//input[@class='validate']", "0636686093", 5000);
 
-        clickOnElement("//div[@class='checkbox']", null, 3000);
+        inputData("//input[@class='validate show-password']", "1234@1234", 5000);
 
-//        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(3));
-//        element = chromeDriver.findElement(By.xpath("//button[@class='button-reset main-btn submit main-btn--green']"));
-//        element.click();
-//        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(3));
-        clickOnElement("//button[@class='button-reset main-btn submit main-btn--green']", null, 3000);
+        clickOnElement("//div[@class='checkbox']", 5000);
 
-        clickOnElement("//*[@id='mm-0']/header/div[2]/div/div[4]/a[1]", null, 3000);
-        try {
-            clickOnElement("//a[@href='https://avic.ua/ua/logout']", null, 3000);
+        clickOnElement("//button[@class='button-reset main-btn submit main-btn--green']", 5000);
+
+        clickOnElement("//*[@id='mm-0']/header/div[2]/div/div[4]/a[1]", 5000);
+//        try {
+        clickOnElement("//a[@href='https://avic.ua/ua/logout']", 5000);
 //            element = chromeDriver.findElement(By.xpath("//a[@href='https://avic.ua/ua/logout']"));
-        } catch (Exception e) {
-        }
-        Assert.assertNull(clickOnElement("//a[@href='https://avic.ua/ua/logout']", null, 0));
+//        } catch (Exception e) {
+//        }
+        Assert.assertNull(clickOnElement("//a[@href='https://avic.ua/ua/logout']", 3000));
     }
 
     @AfterEach
