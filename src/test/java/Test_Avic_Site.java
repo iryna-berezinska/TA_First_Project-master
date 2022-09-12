ff50cd1ad4e9cd28142d03106eb6bac36cff4416
Irina-Berizinska <80217790+Irina-Berizinska@users.noreply.github.com>
2022-09-12 15:15:51 +0300

    fix4

--- src/test/java/Test_Avic_Site.java
+++ src/test/java/Test_Avic_Site.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.jupiter.api.*;
+import org.junit.platform.commons.util.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,44 +11,57 @@ import java.time.Duration;
 public class Test_Avic_Site {
     private static ChromeDriver chromeDriver;
     WebElement element;
+    WebDriverWait wait;
+    WebElement clickOnElement(String query, String text, long millis) throws InterruptedException {
+        WebElement element = chromeDriver.findElement(By.xpath(query));
+        if (StringUtils.isNotBlank(text)) {
+            element.sendKeys(text);
+        }
+        element.click();
 
+        if (millis > 0) {
+            Thread.sleep(millis);
+        }
+
+        return element;
+    }
     @BeforeAll
     public static void setup() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        chromeDriver = new ChromeDriver();
-        chromeDriver.manage().window().maximize();
     }
 
     @BeforeEach
     public void openBrowser() {
+        chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize();
         chromeDriver.get("https://avic.ua/ua");
     }
 
     @Test
     @DisplayName("Check Display Banner")
     public void checkBanner() {
-        new WebDriverWait(chromeDriver, Duration.ofSeconds(35))
-                .until(driver -> driver.findElement(By.xpath("(//div[@id='js_popUp'])")).isDisplayed());
+        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(35));
+                wait.until(driver -> driver.findElement(By.xpath("(//div[@id='js_popUp'])")).isDisplayed());
 
     }
 
     @Test
     @DisplayName("Wait Element")
     public void waitElement() {
-        WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
+    wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/ua/iphone/seriya--iphone-13-promax']")));
     }
 
     @Test
     @DisplayName("Search Button")
     public void searchButton() throws InterruptedException {
-        element = chromeDriver.findElement(By.id("input_search"));
-        element.sendKeys("Apple MacBook Air");
-        element.click();
-        Thread.sleep(4000);
+//        element = chromeDriver.findElement(By.id("input_search"));
+//        element.sendKeys("Apple MacBook Air");
+        clickOnElement("input_search", "Apple MacBook Air", 5);
+//        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
         element = chromeDriver.findElement(By.xpath("//button[@class='button-reset search-btn']"));
         element.click();
-        Thread.sleep(4000);
+        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
     }
 
     @Test
@@ -58,18 +72,18 @@ public class Test_Avic_Site {
         element = chromeDriver.findElement(By.xpath("//input[@class='validate']"));
         element.sendKeys("0636686093");
         element.click();
-        Thread.sleep(3000);
+        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(3));
 
         element = chromeDriver.findElement(By.xpath("//input[@class='validate show-password']"));
         element.sendKeys("1234@1234");
         element.click();
-        Thread.sleep(3000);
+        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(3));
         element = chromeDriver.findElement(By.xpath("//div[@class='checkbox']"));
         element.click();
-        Thread.sleep(3000);
+        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(3));
         element = chromeDriver.findElement(By.xpath("//button[@class='button-reset main-btn submit main-btn--green']"));
         element.click();
-        Thread.sleep(3000);
+        wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(3));
 
         try {
             element = chromeDriver.findElement(By.xpath("//a[@href='https://avic.ua/ua/logout']"));
