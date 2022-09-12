b4bbe5e6250fb25f1d030a08837fa494725ef0dd
Irina-Berizinska <80217790+Irina-Berizinska@users.noreply.github.com>
2022-09-12 11:49:31 +0300

    fix

--- src/test/java/Test_Avic_Site.java
+++ src/test/java/Test_Avic_Site.java
@@ -1,89 +1,87 @@
-
 import org.junit.Assert;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 public class Test_Avic_Site {
+    private static ChromeDriver chromeDriver;
+    WebElement element;
 
     @BeforeAll
     public static void setup() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
     }
 
+    @BeforeEach
+    public void openBrowser() {
+        chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize();
+    }
+
     @Test
     @DisplayName("Check Display Banner")
     public void checkBanner() {
-        ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.manage().window().maximize();
         chromeDriver.get("https://avic.ua/ua");
-
-        new WebDriverWait(chromeDriver, Duration.ofSeconds(35))                                                                  //закрытие окна рекламы вариант 1
+        new WebDriverWait(chromeDriver, Duration.ofSeconds(35))
                 .until(driver -> driver.findElement(By.xpath("(//div[@id='js_popUp'])")).isDisplayed());
-        chromeDriver.close();
+
     }
 
     @Test
     @DisplayName("Wait Element")
     public void waitElement() {
-        ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.manage().window().maximize();
         chromeDriver.get("https://avic.ua/ua");
-
         WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/ua/iphone/seriya--iphone-13-promax']")));
-        chromeDriver.close();
     }
 
     @Test
     @DisplayName("Search Button")
     public void searchButton() throws InterruptedException {
-        ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.manage().window().maximize();
         chromeDriver.get("https://avic.ua/ua");
-
-        WebElement searchButton = chromeDriver.findElement(By.id("input_search"));
-        searchButton.sendKeys("Apple MacBook Air");
-        searchButton.click();
-        Thread.sleep(5000);
-        chromeDriver.close();
+        element = chromeDriver.findElement(By.id("input_search"));
+        element.sendKeys("Apple MacBook Air");
+        element.click();
+        Thread.sleep(4000);
+        element = chromeDriver.findElement(By.xpath("//button[@class='button-reset search-btn']"));
+        element.click();
+        Thread.sleep(4000);
     }
 
     @Test
     @DisplayName("Verify Test")
-    public void verifyButtonName() {
-        ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.manage().window().maximize();
+    public void verifyButtonName() throws InterruptedException {
         chromeDriver.get("https://avic.ua/ua/sign-in");
-//        String verifyAssertNull = null;
-//        Boolean verifyTitle = chromeDriver.getTitle().equalsIgnoreCase("method verifies if the title of the page is null or empty.");
-//        assertNotNull(verifyTitle);
-//        assertNull(verifyAssertNull);
-//        chromeDriver.close();
-        WebElement searchButton = chromeDriver.findElement(By.className("//*[@class='width-auto']"));
-//        chromeDriver.findElement(By.className(".//*[@class='validate show-password']"));
-        searchButton.sendKeys("0635699999");
-        searchButton.click();
+        element = chromeDriver.findElement(By.xpath("//input[@class='validate']"));
+        element.sendKeys("0636686093");
+        element.click();
+        Thread.sleep(3000);
 
-//        chromeDriver.findElement(By.className("//*[@class='validate show-password']")).sendKeys("Test@123");
-//        chromeDriver.findElement(By.className("//*[@class='button-reset main-btn submit main-btn--green']")).click();
-//        try{
-//            element = chromeDriver.findElement (By.xpath(".//*[@id='account_logout']/a"));
-//        }catch (Exception e){
-//        }
-//        Assert.assertNotNull(searchButton);
-//        System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
+        element = chromeDriver.findElement(By.xpath("//input[@class='validate show-password']"));
+        element.sendKeys("1234@1234");
+        element.click();
+        Thread.sleep(3000);
+        element = chromeDriver.findElement(By.xpath("//div[@class='checkbox']"));
+        element.click();
+        Thread.sleep(3000);
+        element = chromeDriver.findElement(By.xpath("//button[@class='button-reset main-btn submit main-btn--green']"));
+        element.click();
+        Thread.sleep(3000);
+
+        try {
+            element = chromeDriver.findElement(By.xpath("//a[@href='https://avic.ua/ua/logout']"));
+        } catch (Exception e) {
+        }
+        Assert.assertNotNull(element);
     }
+
+    @AfterEach
+    public void closeBrowser() {
+        chromeDriver.close();
     }
+}
 
